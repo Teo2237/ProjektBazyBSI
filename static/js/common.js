5f5cd0d5b18b90036e23739bc60db7b44789adb9
Teo2237 <127605714+Teo2237@users.noreply.github.com>
2025-06-14 21:16:33 +0000

    Dodanie rozwijanego menu nawigacyjnego przy małej szerokości okna (mobile)

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,16 @@
-if (document.getElementById('current-year')) {
-    document.getElementById('current-year').textContent = new Date().getFullYear();
-}
+// Kod do obsługi menu hamburgera
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const mainNavLinks = document.getElementById('main-nav-links');
+
+    if (hamburgerBtn && mainNavLinks) {
+        hamburgerBtn.addEventListener('click', () => {
+            mainNavLinks.classList.toggle('mobile-active');
+        });
+    }
+
+    // Istniejący kod do stopki
+    if (document.getElementById('current-year')) {
+        document.getElementById('current-year').textContent = new Date().getFullYear();
+    }
+});
